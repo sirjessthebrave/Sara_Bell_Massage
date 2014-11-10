f053f462b79215db24f791e93e64dc5dc510e7a2
Sir Jess the Brave <jess.bell317@gmail.com>
2014-11-10 18:38:35 -0500

    11/10 changes
    fix the hover pics, added intro paragraph, grew footer

---
+++ script.js
@@ -0,0 +1,5 @@
+onload: opacity of the backgroun image fades slowly from black to the image
+
+when the hero reaches the top of the page, nav switches to a static top bar that does not move as the rest of the page scrolls
+
+3 main images: onhover: a grey box with light opacity takes over the image with white text.  the whole image links to another page.
