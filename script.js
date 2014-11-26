ba9496186891243bf448b463d417ad2b9757005d
Sir Jess the Brave <jess.bell317@gmail.com>
2014-11-25 19:40:52 -0500

    Nov 25th updates
    add about and services pages, add in links, white background

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-onload: opacity of the backgroun image fades slowly from black to the image
+// sticky nav for home page
 
-when the hero reaches the top of the page, nav switches to a static top bar that does not move as the rest of the page scrolls
+$("#sticky_navigation").stick_in_parent()
 
-3 main images: onhover: a grey box with light opacity takes over the image with white text.  the whole image links to another page.
+//end of sticky nav for home page
+
 
 
 // Google Map for Contact Page
 
-var mapCanvas
+// var mapCanvas
 
-function initialize() {
-    var mapCanvas = document.getElementById('map_canvas');
-    var mapOptions = {
-      center: new google.maps.LatLng(44.5403, -78.5463),
-      zoom: 8,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-  }
+// function initialize() {
+//     var mapCanvas = document.getElementById('map_canvas');
+//     var mapOptions = {
+//       center: new google.maps.LatLng(44.5403, -78.5463),
+//       zoom: 8,
+//       mapTypeId: google.maps.MapTypeId.ROADMAP
+//     }
+//     var map = new google.maps.Map(mapCanvas, mapOptions);
+//   }
 
 // END Google Map for Contact Page
+
